08945047a9ba3329d64d3806211db16519db33b4
0xClandestine <clandestine.eth@proton.me>
2022-12-09 15:46:17 -0600

    ♻️setup test

--- test/DualToken.t.sol
+++ test/DualToken.t.sol
@@ -5,9 +5,39 @@ import "forge-std/Test.sol";
 import {ERC20Child} from "../src/ERC20Child.sol";
 import {ERC1155Parent} from "../src/ERC1155Parent.sol";
 
-contract DualTokenTest is Test {
-    // ERC1155Parent parent;
-    // ERC20Child child;
+contract MockERC20Child is ERC20Child {
+    constructor(address _parent, uint256 _id)
+        ERC20Child("Mock name", "Mock symbol", 18, _parent, _id)
+    {}
+}
 
-    function setUp() public {}
+contract MockERC1155Parent is ERC1155Parent {
+    function mint(address account, uint256 id, uint256 amount) external {
+        _mint(account, id, amount, new bytes(0));
+    }
+
+    function burn(address account, uint256 id, uint256 amount) external {
+        _burn(account, id, amount);
+    }
+
+    function uri(uint256 id)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        return "";
+    }
+}
+
+contract DualTokenTest is Test {
+    MockERC1155Parent parent;
+    MockERC20Child child;
+    uint256 constant id = 420;
+
+    function setUp() public {
+        parent = new MockERC1155Parent();
+        child = new MockERC20Child(address(parent), id);
+    }
 }
